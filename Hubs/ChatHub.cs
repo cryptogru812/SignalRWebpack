7c0c64685fe77e77ecfbcf282a14922ee4f51125
cryptogru812 <wongaiden812@gmail.com>
2025-03-17 10:36:47 -0400

    Add project files.

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRWebpack.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task newMessage(long username, string messsage) =>
+            await Clients.All.SendAsync("messageReceived", username, messsage);
+    }
+}
